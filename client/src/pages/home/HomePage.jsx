9e583e78434a395830c3e3a317b3a2481b654b5e
rassim2458 <saddekrassim@gmail.com>
2024-05-23 06:50:33 +0100

    06.50

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,46 +1,58 @@
+import { useState } from "react";
+
+import Posts from "../../components/common/Posts";
+import CreatePost from "./CreatePost";
+import RightPanel from "../../components/common/RightPanel";
+import { ProfileCard } from "../../components/common/ProfileCard ";
+
 const HomePage = () => {
 	const [feedType, setFeedType] = useState("forYou");
 
 	return (
-		<div className="flex flex-col md:flex-row justify-center items-start mt-20">
-			<div className="w-full md:w-3/4 max-w-screen-lg">
-				{/* Header */}
-				<div className="flex">
+		<>
+			<div className='flex-[4_4_0] mr-20 ml-20'>
+						{/* Header */}
+						
+				<div className='flex w '>
 					<div
 						className={
-							"tab" + (feedType === "forYou" ? " active" : "")
+							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
 						}
 						onClick={() => setFeedType("forYou")}
 					>
 						For you
+						{feedType === "forYou" && (
+							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+						)}
 					</div>
 					<div
-						className={
-							"tab" + (feedType === "following" ? " active" : "")
-						}
+						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
 						onClick={() => setFeedType("following")}
 					>
 						Following
+						{feedType === "following" && (
+							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+						)}
 					</div>
 				</div>
-
-				{/* CREATE POST INPUT */}
+				
+				{/*  CREATE POST INPUT */}
 				<CreatePost />
 
 				{/* POSTS */}
 				<Posts feedType={feedType} />
+				
 			</div>
-			<div className="w-full md:w-1/4 mt-8 md:mt-0">
-				<ProfileCard
-					imageSrc="/avatar-placeholder.png"
-					username="issam"
-					email="issam@example.com"
-					description="web app dev ..."
-				/>
-				<RightPanel />
+			<div className=""style={{ height: '200px' }}>
+			<ProfileCard
+				imageSrc="/avatar-placeholder.png"
+				username="issam"
+				email="issam@example.com"
+				description="web app dev ..."
+			/>  
+			 <RightPanel />  
 			</div>
-		</div>
+		</>
 	);
 };
-
 export default HomePage;
